3d0a4fd8e59c8e650cd266cd682fa16c1621725e
Oleg Shvedov <ol.shvedov@gmail.com>
2017-11-19 13:03:00 +0300

    Chat auto scroll while new message receives

--- common/components/MessageList.js
+++ common/components/MessageList.js
@@ -1,44 +1,67 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 
-const MessageList = ({ messages }) => {
-  const list = messages.map((message, i) => {
-    if (!message.username && message.service) {
+export default class MessageList extends React.Component {
+  static defaultProps = {
+    messages: [],
+  };
+
+  static propTypes = {
+    messages: PropTypes.arrayOf(
+      PropTypes.shape({
+        username: PropTypes.string,
+        text: PropTypes.string.isRequired,
+        service: PropTypes.bool,
+      }),
+    ),
+  };
+
+  componentDidUpdate() {
+    this.scrolled = this.messagesContainer.scrollHeight -
+      (this.messagesContainer.scrollTop + this.messagesContainer.offsetHeight) >= 50;
+
+    if (this.scrolled) {
+      return;
+    }
+    this.messagesContainer.scrollTop = this.messagesContainer.scrollHeight;
+  }
+
+  render() {
+    const list = this.props.messages.map((message, i) => {
+      if (!message.username && message.service) {
+        return (
+          <li className={classNames('log', 'message')} key={i}>
+            <span className={classNames('log', 'message-body')}>{message.text}</span>
+          </li>
+        );
+      }
       return (
-        <li className="log message" key={i}>
-          <span className="log message-body">{message.text}</span>
+        <li className="message" key={i}>
+          <span className="username">{message.username}</span>
+          <span className="message-body">{message.text}</span>
         </li>
       );
-    }
+    });
+
     return (
-      <li className="message" key={i}>
-        <span className="username">{message.username}</span>
-        <span className="message-body">{message.text}</span>
-      </li>
+      <ul
+        className="messages"
+        ref={(node) => {
+          this.messagesContainer = node;
+        }}
+      >
+        {list.length
+          ? list
+          : (
+            <li className={classNames('log', 'message')}>
+              <span className={classNames('log', 'message-body')}>Here you can chat while playing</span>
+            </li>
+          )
+        }
+      </ul>
     );
-  });
-
-  return (
-    <ul className="messages">
-      {list}
-    </ul>
-  );
-};
-
-MessageList.defaultProps = {
-  messages: [],
-};
-
-MessageList.propTypes = {
-  messages: PropTypes.arrayOf(
-    PropTypes.shape({
-      username: PropTypes.string,
-      text: PropTypes.string.isRequired,
-      service: PropTypes.bool,
-    }),
-  ),
-};
-
-export default MessageList;
+  }
+}
